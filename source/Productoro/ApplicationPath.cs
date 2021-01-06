27aeb1273d9793bcd77f6d6bcf61843714c60378
Ryan Kroese <rykroese@live.com>
2021-01-06 11:03:13 -0600

    Nullability fixes.

--- source/Productoro/ApplicationPath.cs
+++ source/Productoro/ApplicationPath.cs
@@ -7,16 +7,16 @@ namespace Productoro
 {
     public static class ApplicationPath
     {
-        private static Lazy<string> _applicationFile { get; } = new Lazy<string>(() =>
+        private static Lazy<string?> _applicationFile { get; } = new Lazy<string?>(() =>
         {
             using var process = System.Diagnostics.Process.GetCurrentProcess();
-            return process.MainModule.FileName;
+            return process.MainModule?.FileName;
         });
 
         private static Lazy<string> _applicationDirectory { get; } =
            _applicationFile.Select(f => Path.GetDirectoryName(f).AsOption().ValueOrFailure("Directory does not exist for file."));
 
-        public static string File => _applicationFile.Value;
+        public static string? File => _applicationFile.Value;
 
         public static string Directory => _applicationDirectory.Value;
     }
