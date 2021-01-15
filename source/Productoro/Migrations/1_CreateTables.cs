a0d498e160cde1f1f749cd15468bf2352cf954f0
Ryan Kroese <rykroese@live.com>
2021-01-15 14:17:46 -0600

    Fixed event table schema and added indices.

--- source/Productoro/Migrations/1_CreateTables.cs
+++ source/Productoro/Migrations/1_CreateTables.cs
@@ -10,17 +10,44 @@ namespace Productoro.Migrations
             Create
                 .Table("Events")
                 .WithColumn("Id").AsGuid().NotNullable().PrimaryKey().Identity()
-                .WithColumn("Aggregate").AsString().NotNullable()
-                .WithColumn("InstanceId").AsString().NotNullable()
+                .WithColumn("AggregateType").AsString().NotNullable()
+                .WithColumn("AggregateId").AsGuid().NotNullable()
                 .WithColumn("Type").AsString().NotNullable()
                 .WithColumn("Body").AsString()
                 .WithColumn("Timestamp").AsDateTimeOffset().NotNullable();
+
+            Create
+                .Index("ix_AggregateType")
+                .OnTable("Events")
+                .OnColumn("AggregateType")
+                .Descending()
+                .WithOptions().NonClustered();
+
+            Create
+                .Index("ix_AggregateId")
+                .OnTable("Events")
+                .OnColumn("AggregateId")
+                .Descending()
+                .WithOptions().NonClustered();
+
+            Create
+                .Index("ix_Type")
+                .OnTable("Events")
+                .OnColumn("Type")
+                .Descending()
+                .WithOptions().NonClustered();
+
+            Create
+                .Index("ix_Timestamp")
+                .OnTable("Events")
+                .OnColumn("Timestamp")
+                .Descending()
+                .WithOptions().NonClustered();
         }
 
         public override void Down()
         {
             Delete.Table("Events");
         }
-
     }
 }
